f29cabf5f38c19f9db237d1223132a88b14b7d4a
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-22 00:48:44 -0400

    Time: 490 ms (46.7%), Space: 38.7 MB (41.67%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -7,44 +7,38 @@ class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next: 
             return head
-        return self.sort(head)
 
-    def sort(self, head):
-        def merge(left, right):
-            dummy = ListNode(-1)
-            curr = dummy
+        mid = self.find_mid(head)            
+        left = self.sortList(head)
+        right = self.sortList(mid)
 
-            while left and right:
-                if left.val < right.val:
-                    curr.next = left
-                    left = left.next
-                else:
-                    curr.next = right
-                    right = right.next
-                curr = curr.next
+        return self.merge(left, right)
 
-            curr.next = left if left else right
+    def merge(self, left, right):
+        dummy = ListNode(-1)
+        curr = dummy
 
-            return dummy.next
-        
-        def find_mid(head):
-            prev = None
-            fast = slow = head
+        while left and right:
+            if left.val < right.val:
+                curr.next = left
+                left = left.next
+            else:
+                curr.next = right
+                right = right.next
+            curr = curr.next
 
-            while fast and fast.next:
-                prev = slow
-                slow = slow.next
-                fast = fast.next.next
+        curr.next = left if left else right
+
+        return dummy.next
+
+    def find_mid(self, head):
+        prev = None
+        fast = slow = head
+
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
             
-            if prev:
-                prev.next = None
-            return slow
-
-        mid = find_mid(head)
-        if head == mid:
-            return head
-            
-        left = self.sort(head)
-        right = self.sort(mid)
-
-        return merge(left, right)
+        prev.next = None
+        return slow
