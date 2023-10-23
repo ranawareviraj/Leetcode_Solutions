301a9cc9a6d42388a8b5bd8ff4a1eaceba99312a
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-23 19:39:22 -0400

    Time: 45 ms (41.35%), Space: 16.3 MB (9.87%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -6,10 +6,9 @@ class Solution:
         dp = [0] * (n + 1)
         dp[1] = 1
 
-        for i in range(n + 1):
-            if (i + 1) <= (n):
-               dp[i + 1] += dp[i] 
-            if (i + 2) <= (n):
-                dp[i + 2] += dp[i]
+        for i in range(n - 1):
+            dp[i + 1] += dp[i] 
+            dp[i + 2] += dp[i]
         
+        dp[n] += dp[n-1]
         return dp[n]
