cad3793ae74a2e86af6561c45718a51da7ae60b8
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-23 13:45:53 -0400

    Time: 40 ms (62.08%), Space: 16 MB (94.37%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -3,13 +3,9 @@ class Solution:
         if n <= 1:
             return n
         
-        dp = [0] * (n + 1)
-        dp[0] = 0   # explicit 0 assignment for redability
-        dp[1] = 1
+        prev_one = 0  
+        prev_two = 1
 
-        for i in range(2, (n + 1)):
-            # Recurrence Relation
-            # F(n) = F(n - 1) + F(n - 2), for n > 1
-            dp[i] = dp[i - 1] + dp[i - 2]
-
-        return dp[n]
+        for i in range(2, (n + 2)):
+            prev_two, prev_one = prev_one, prev_one + prev_two
+        return prev_one
