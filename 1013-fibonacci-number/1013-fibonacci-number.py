b842949b4f72b36ce08e619384812695522219ec
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-23 13:50:00 -0400

    Time: 39 ms (65.71%), Space: 16.2 MB (41.93%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -3,9 +3,10 @@ class Solution:
         if n <= 1:
             return n
         
-        prev_one = 0  
-        prev_two = 1
+        prev_two = 0
+        prev_one = 1 
+        
+        for i in range(2, (n + 1)):
+            prev_two, prev_one = prev_one, (prev_one + prev_two)
 
-        for i in range(2, (n + 2)):
-            prev_two, prev_one = prev_one, prev_one + prev_two
         return prev_one
