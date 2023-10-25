6e1cefae255e6deec005f070df139c689c6d20c5
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-25 13:28:12 -0400

    Time: 2924 ms (10.58%), Space: 17.6 MB (6.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,17 +1,31 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        sub_seq = [nums[0]]
+        def dp(i, memo):
+            # If result in cache, return cached result - Memoization
+            if i in memo:
+                return memo[i]
 
-        for i in range(1, n):
-            num = nums[i]
+            # Base case
+            ans = 1
 
-            if num > sub_seq[-1]:
-                sub_seq.append(num)
-            else:
-                j = 0
-                while num > sub_seq[j]:
-                    j += 1
-                sub_seq[j] = num
+            # ecursively find 
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    # If number at j is less than number at i,
+                    # so we have length = len(j) + 1(for ith number)
+                    ans = max(ans, dp(j, memo) + 1)
+            
+            # Add result to cache - Memoization
+            memo[i] = ans       
+            return ans
         
-        return len(sub_seq)
+        max_length = 1
+        n = len(nums)
+        memo = {}
+
+        # For each index of an array,
+        # Find out max_length of increasing subsequence ending at current index
+        for i in range(n):
+            max_length = max(max_length, dp(i, memo))
+        
+        return max_length
