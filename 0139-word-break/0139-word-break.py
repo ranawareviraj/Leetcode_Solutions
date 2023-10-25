b977ceb311fae0af1d5f7da42b34bff84a65a2ed
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-25 15:04:26 -0400

    Time: 48 ms (34.44%), Space: 16.2 MB (88.55%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,21 +1,15 @@
 class Solution:
-    def wordBreak(self, s: str, word_dict: List[str]) -> bool:
-        def can_construct(s, word_dict, memo):
-            if s == "":
-                return True
-            if s in memo:
-                return memo[s]
-            
-            for word in word_dict:
-                if s.startswith(word):
-                    new_s = s[len(word):]
-                    if can_construct(new_s, word_dict, memo):
-                        memo[new_s] =  True
-                        return True
-            
-            memo[s] = False
-            return False
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [ False ] * (n + 1)
+        dp[0] = True
 
-        memo = {}
-        return can_construct(s, word_dict, memo)
+        for i in range(n + 1):
+            if dp[i] == True:
+                for word in wordDict:
+                    m = len(word)
+                    new_target = s[i : i + m]
+                    if new_target.startswith(word):
+                        dp[i + m] = True
         
+        return dp[n]
