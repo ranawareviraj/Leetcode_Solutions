062dd4ecdf7f920dbc4ad37e8f0535a6158652f8
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-26 21:09:30 -0400

    Time: 74 ms (27.73%), Space: 19.3 MB (13.85%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,49 +1,16 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        def dp(index, row, grid, memo):
-            if (index, row) in memo:
-                return memo[(index, row)]
-            if row == 0:
-                return grid[0][0]
-            if index < 0:
-                return math.inf
+
+        def dp(row, col, memo):
+            if (row, col) in memo:
+                return memo[(row, col)]
             
-            up = dp(index, row - 1, grid, memo)
-            diagonal = dp(index - 1, row - 1, grid, memo)
-
-            memo[(index, row)] = grid[row][index] + min(up, diagonal)
-            return memo[(index, row)]
-
+            min_path_sum = triangle[row][col]
+            if row < m - 1:
+                min_path_sum += min(dp(row + 1, col, memo), dp(row + 1, col + 1, memo))
+            
+            memo[(row, col)] = min_path_sum
+            return min_path_sum
+        
         m = len(triangle)
-        n = len(triangle[m - 1])
-        grid = [[math.inf] * n for _ in range(m)]
-        
-        for row in range(m):
-            for col in range(len(triangle[row])):
-                grid[row][col] = triangle[row][col]
-        
-        ans = math.inf
-        for index in range(n):
-            ans = min(ans, dp(index, m - 1, grid, {}))
-        return ans
-
-'''
-[
-    [2],       [0][0]
-    [3,4],     [1][0], [1][1]    
-    [6,5,7],   [2][0], [2][1], [2][2]
-    [4,1,8,3]  [3][0]
-]
-
-[
-    [inf, inf, inf, inf], 
-    [inf, inf, inf, inf], 
-    inf, inf, inf, inf], 
-    [inf, inf, inf, inf]]
-
-[
-    [2, inf, inf, inf], 
-    [3, 4, inf, inf], 
-    [6, 5, 7, inf], 
-    [4, 1, 8, 3]]
-'''
+        return dp(0, 0, {})
