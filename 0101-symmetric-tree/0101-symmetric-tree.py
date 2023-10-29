10be467a0bfb991f7f45b320104712b627c2194b
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-28 23:16:35 -0400

    Time: 48 ms (13.52%), Space: 16.3 MB (83.34%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,38 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        queue = collections.deque()
-        # Start comparison from roots left and right child
-        queue.append(root.left)
-        queue.append(root.right)
+        def is_symmetric(p, q):
+            if not (p or q):
+                return True
 
-        while(queue):
-            sub_tree1 = queue.popleft()
-            sub_tree2 = queue.popleft()
-            
-            # If both the current nodes are null, continue validating next set of nodes
-            if (not sub_tree1) and (not sub_tree2):
-                continue
-
-            # If either of the node is null, tree is not symmetric
-            if (not sub_tree1) or (not sub_tree2):
+            if not p or not q:
                 return False
-            
-            # If values of both the current nodes is not same, tree is not symmetric 
-            if sub_tree1.val != sub_tree2.val:
-                return False
-                
-            # Add nodes of both subtrees from opposite sides
-            queue.append(sub_tree1.left)
-            queue.append(sub_tree2.right)
-            queue.append(sub_tree1.right)
-            queue.append(sub_tree2.left)
 
-        # If we reached till end without validation failure, tree is symmetric 
-        return True
-    
-# Notes:
-# left child node = left sub-tree
-# right child node = right sub-tree
+            if p.val != q.val:
+                return False
+
+            return is_symmetric(p.left, q.right) and is_symmetric(p.right, q.left)
+
+        return is_symmetric(root.left, root.right)
