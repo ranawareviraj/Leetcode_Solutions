e4ed7b40d1ac2aae4a7518e7de68c173db486c40
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-28 21:22:27 -0400

    Time: 37 ms (66.95%), Space: 16.4 MB (28.57%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -4,22 +4,30 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # If both trees are Null, they are same tree
-        if not p and not q:
-            return True
-        
-        # Else if either of the tree is Null, they are not same tree
-        if not p or not q:
-            return False
-        
-        # Else values of respective nodes aren't same, they are not same tree
-        if p.val != q.val:
-            return False
-        
-        # For trees to be same, the left and right subtrees must be same
-        is_same_tree_left = self.isSameTree(p.left, q.left)
-        is_same_tree_right = self.isSameTree(p.right, q.right)
 
-        return is_same_tree_left and is_same_tree_right
+        def validate_trees(p, q):
+            if p == q == None:
+                return True
+
+            if (not p or not q):
+                return False
+
+            if p.val != q.val:
+                return False
+
+            return True
+
+        queue = deque([(p, q)])
+        while queue:
+            p, q = queue.popleft()
+            if validate_trees(p, q) == False:
+                return False
+            
+            if q:
+                queue.append((p.left, q.left))
+                queue.append((p.right, q.right))
+        
+        return True
