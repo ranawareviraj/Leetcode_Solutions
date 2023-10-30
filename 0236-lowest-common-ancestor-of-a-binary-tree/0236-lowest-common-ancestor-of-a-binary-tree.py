70c7cc87994846dd1d4f84053d2b0c19050f5bc5
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-29 21:45:19 -0400

    Time: 1441 ms (5.01%), Space: 615.5 MB (5%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -18,16 +18,16 @@ class Solution:
                 find_paths(node.left, p, q, new_path)
                 find_paths(node.right, p, q, new_path)
         
+        # Find paths from root to node p and q
         paths = []
         find_paths(root, p, q, [])
         
-        l1, l2 = len(paths[0]), len(paths[1])
-        prev1, prev2 = paths[0][0], paths[1][0]
-        n = min(l1, l2)
-        
+        # Find the first node after which their paths diverge
+        n = min(len(paths[0]), len(paths[1]))
+        prev = root
         for i in range(n):
             if paths[0][i].val != paths[1][i].val:
-                return prev1
-            prev1, prev2 = paths[0][i], paths[1][i]
+                return prev
+            prev = paths[0][i] # or paths[1][i]
         
-        return prev1
+        return prev
