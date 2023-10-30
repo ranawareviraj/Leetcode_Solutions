7e9b82eb825085114c2bfa67f94d72175a5095b9
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-10-30 18:36:20 -0400

    Time: 47 ms (99.58%), Space: 21.1 MB (80.45%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,25 +6,30 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def build_tree(preorder, inorder, index_map):
-            if not preorder or not inorder:
+        def build_tree(left, right):
+            nonlocal preorder_index
+
+            if left > right:
                 return None
             
-            # First node in preorder traversal is root node. 
-            # Find its index in inorder traversal list - total nodes in left subtree
-            node = TreeNode(preorder[0])
-            pivot = inorder.index(preorder[0])
+            node_val = preorder[preorder_index]
+            node = TreeNode(node_val)
+
+            pivot = index_map[node_val]
+            preorder_index += 1
+
+            # print(node_val)
 
             # Send left subtree elements to the left of current node and
             # Send right subtree elements to the right of current node
-            node.left = build_tree(preorder[1 : pivot + 1], inorder[ : pivot], index_map)
-            node.right = build_tree(preorder[pivot + 1 : ], inorder[pivot + 1 : ], index_map)
+            node.left = build_tree(left, pivot - 1)
+            node.right = build_tree(pivot + 1, right)
             
             return node
-         
-        # Build index map for nodes in inorder traversal list for constant time lookup
+
         index_map = {}
-        for node, index in enumerate(inorder):
-            index_map[node] = index
-        
-        return build_tree(preorder, inorder, index_map)
+        for index, node in enumerate(inorder):
+            index_map[ node ] = index
+
+        preorder_index = 0
+        return build_tree(0, len(inorder) - 1)
