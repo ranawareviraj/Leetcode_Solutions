d7b4d5ae754028371a95ee599ee0cbbb57ae7b88
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-01 01:10:35 -0400

    Time: 780 ms (95.04%), Space: 16.7 MB (60.76%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,37 +1,16 @@
   
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        def dp(target, memo):
-            if target in memo:
-                return memo[target]
-            if target < 0:
-                return -1
-            if target == 0:
-                return 0
-            
-            count = math.inf
-            for coin in coins:
-                new_count = dp(target - coin, memo)
-                if new_count != -1:
-                    count = min(count, new_count + 1)
-            
-            memo[target] = count        
-            return -1 if (count == math.inf) else count
-        
-        memo = {}
-        return dp(amount, memo)
-
-
-'''
-        dp = [ math.inf ] * (amount + 1)
+        n = amount + 1
+        dp = [ math.inf ] * (n)
         dp[0] = 0 
 
-        for i in range(amount + 1):
-            for coin in coins:
-                coint_count = dp[i] + 1
-                if (i + coin) <= amount:
-                    dp[i + coin] = min(coint_count, dp[i + coin])
+        for i in range(n - 1):
+            if dp[i] != math.inf:
+                coin_count = dp[i] + 1
+                for coin in coins:
+                    next_amount = i + coin
+                    if (next_amount) <= amount:
+                        dp[next_amount] = min(dp[next_amount], coin_count)
         
         return dp[amount] if (dp[amount] != math.inf) else (-1)
-'''
