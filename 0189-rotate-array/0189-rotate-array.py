9ac7915b2ce4be66a18e9217fe94c989bbc2143a
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-01 14:35:27 -0400

    Time: 183 ms (61.62%), Space: 27.8 MB (46.32%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,18 +1,14 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        def reverse_arr_elements(arr, start, end):
+            while start < end:
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+                end -= 1
+
         n = len(nums)
         k = k % n # If k is greater than n, effectively we need to copy only n % k elements
         
-        # Get last k elements from the list
-        end_list = [0] * k
-        for i in range(k):
-            end_list[k - i - 1] = nums[n - 1 - i]
-
-        # Move remaining elements by k positions(to the end of the list)
-        pivot = n - 1 - k
-        for i in range( pivot, -1, -1 ):
-            nums[i + k] = nums[i]
-
-        # Lastly, copy last k elements at the start of the list
-        for i in range(k):
-            nums[i] = end_list[i]
+        reverse_arr_elements(nums, 0, n - 1)
+        reverse_arr_elements(nums, 0, k - 1)
+        reverse_arr_elements(nums, k, n - 1)
