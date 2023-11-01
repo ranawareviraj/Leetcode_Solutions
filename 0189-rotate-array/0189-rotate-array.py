133684534cec3976e5d60e297d9480926c83a83d
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-01 14:28:02 -0400

    Time: 190 ms (41.92%), Space: 27.7 MB (46.32%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,16 +1,18 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         n = len(nums)
-        k = k % n
-
+        k = k % n # If k is greater than n, effectively we need to copy only n % k elements
+        
+        # Get last k elements from the list
         end_list = [0] * k
         for i in range(k):
             end_list[k - i - 1] = nums[n - 1 - i]
-                
-        j = 0
-        for i in range(n - 1 - k, -1, -1):
-            nums[n - 1 - j] = nums[i]
-            j += 1
 
+        # Move remaining elements by k positions(to the end of the list)
+        pivot = n - 1 - k
+        for i in range( pivot, -1, -1 ):
+            nums[i + k] = nums[i]
+
+        # Lastly, copy last k elements at the start of the list
         for i in range(k):
             nums[i] = end_list[i]
