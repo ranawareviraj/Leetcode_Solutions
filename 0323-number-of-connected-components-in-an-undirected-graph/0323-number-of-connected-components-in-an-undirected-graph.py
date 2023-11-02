df44a2950e8b504224ba51f75e454c043a7175c4
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-02 16:03:10 -0400

    Time: 102 ms (47.56%), Space: 19.8 MB (24.52%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,30 +1,32 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        # build graph
-        def build_graph(edges):
-            graph = defaultdict(list)
-            for (x, y) in edges:
-                graph[x].append(y)
-                graph[y].append(x)
-            return graph
-
-        def dfs(node, graph, visited):
-            if node in visited:
-                return False
+        # Build graph
+        graph = collections.defaultdict(list)
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x) 
             
-            visited.add(node)
+        # DFS starting from a node
+        def dfs(node):
+            # Base case : If node is already visited, its component is already explored
+            # So just return
+            if node in seen:
+                return
+            
+            # If we come to this line means the node is being visited for the first time
+            seen.add(node)
 
+            # Recursively explore all the nodes part of current component
             for neighbor in graph[node]:
-                dfs(neighbor, graph, visited)
-            
-            return True
-
-        graph = build_graph(edges)
-        visited = set()
-        count = 0
-
-        for node in range(n):
-            if dfs(node, graph, visited):
-                count += 1
+                dfs(neighbor)
         
-        return count
+        seen = set()
+        number_of_components = 0
+
+        # Visit all nodes in a graph in DFS manner
+        for node in range(n):
+            if node not in seen:
+                number_of_components += 1
+                dfs(node)
+
+        return number_of_components
