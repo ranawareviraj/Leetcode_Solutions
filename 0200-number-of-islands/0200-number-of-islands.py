0b27d195c72e737f57842ec940d59e22aaebe515
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-03 01:01:05 -0400

    Time: 315 ms (36.27%), Space: 24 MB (35.61%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,15 +3,16 @@ class Solution:
         def valid(row, col):
             return 0 <= row < m and 0 <= col < n and grid[row][col] == "1"
         
-        def bfs(r, c):
-            queue = collections.deque([(r, c)])
-            while queue:
-                row, col = queue.popleft()
+        def dfs(r, c):
+            stack = [(r, c)]
+            while stack:
+                row, col = stack.pop()
+                seen.add((row, col))
+
                 for dx, dy in directions:
                     next_row, next_col = row + dy, col + dx
                     if valid(next_row, next_col) and (next_row, next_col) not in seen:
-                        seen.add((next_row, next_col))
-                        queue.append((next_row, next_col))
+                        stack.append((next_row, next_col))
 
         m, n = len(grid), len(grid[0])        
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
@@ -23,6 +24,6 @@ class Solution:
                 if grid[row][col] == "1" and (row, col) not in seen:
                     number_of_islands += 1
                     seen.add((row, col))
-                    bfs(row, col)
+                    dfs(row, col)
         
         return number_of_islands
