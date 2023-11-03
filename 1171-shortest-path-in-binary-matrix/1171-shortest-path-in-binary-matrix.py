cc551c9b530484972465700e0142d9061fec9b59
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-03 18:52:06 -0400

    Time: 591 ms (40.89%), Space: 18 MB (21.42%) - LeetHub

---
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -0,0 +1,38 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        def is_valid(row, col):
+            return (-1 < row < m) and (-1 < col < n) and (grid[row][col] == 0)
+
+        # Base case: If start val is 1, we can't reach the end
+        if grid[0][0] == 1:
+            return -1
+        
+        m = len(grid)
+        n = len(grid[0])
+        # Possible neighbors if they in bounds
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1), (-1, 1), (1, -1), (-1, -1), (1, 1)]
+        
+        # Initialize BFS queue and set of visited node
+        queue = deque([(0, 0)])
+        seen = { (0, 0) }
+        level = 0
+
+        # BFS
+        while queue:
+            level += 1
+            level_size = len(queue)
+            for _ in range(level_size):
+                row, col = queue.popleft()
+
+                if (row, col) == (m - 1, n - 1):
+                    return level
+
+                for dx, dy in directions:
+                    next_row = row + dy
+                    next_col = col + dx
+
+                    if is_valid(next_row, next_col) and (next_row, next_col) not in seen:
+                        seen.add((next_row, next_col))
+                        queue.append((next_row, next_col))
+            
+        return -1
