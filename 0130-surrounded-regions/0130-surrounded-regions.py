719b8d000d5fafc05cd60762dda503e73f9e3710
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-03 23:51:48 -0400

    Time: 140 ms (47.33%), Space: 18.2 MB (50.82%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,12 +4,13 @@ class Solution:
         def is_valid(r, c):
             return 0 <= r < m and 0 <= c < n
 
+        # DFS from boundaries marking all zeroes
         def dfs(row, col):
             if not is_valid(row, col) or board[row][col] != 'O':
                 return
 
             board[row][col] = '#'
-            
+
             for dx, dy in directions:
                 nr, nc = row + dy, col + dx
                 dfs(nr, nc)
@@ -17,9 +18,10 @@ class Solution:
         directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         m, n = len(board), len(board[0])
 
-        # Mark 'O's on the edges with '#'
+        # Mark 'O's on the edges (and all zeroes of that connected component) with '#' 
         for row in range(m):
             for col in range(n):
+                # Start DFS from edge cell with value 0
                 if (row == 0 or col == 0 or row == m - 1 or col == n - 1) and board[row][col] == 'O':
                     dfs(row, col)
 
