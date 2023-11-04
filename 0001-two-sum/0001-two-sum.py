2e8907054d3f30500de2022fe5c48ca246cbec76
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-04 01:41:37 -0400

    Time: 66 ms (60.92%), Space: 17.6 MB (38.3%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        n = len(nums)
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in hash_map:
+                return [hash_map[complement], i]
+            
+            hash_map[nums[i]] = i
+        
+        return [-1, -1]
