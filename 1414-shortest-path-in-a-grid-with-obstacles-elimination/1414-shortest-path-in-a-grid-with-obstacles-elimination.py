be511b97aaa0b6476ccbf46dd4679f1e2174a5f2
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-03 21:20:43 -0400

    Time: 574 ms (32.11%), Space: 24.6 MB (19.22%) - LeetHub

---
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -0,0 +1,32 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        def in_bounds(row, col):
+            return (-1 < row < m) and (-1 < col < n)
+
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        m, n = len(grid), len(grid[0])
+        seen = set()
+        seen.add((0, 0, k))
+        queue = deque([(0, 0, k)])
+        level = 0
+        
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                r, c, obstacles = queue.popleft()
+    
+                if (r, c) == (m - 1, n - 1):
+                    return level 
+                
+                for dx, dy in directions:
+                    nr, nc = r + dy, c + dx
+                    if in_bounds(nr, nc):
+                        next_obstacles = obstacles - grid[nr][nc] 
+
+                        if (nr, nc, next_obstacles) not in seen and (next_obstacles >= 0):
+                            seen.add((nr, nc, next_obstacles))
+                            queue.append((nr, nc, next_obstacles))
+
+            level += 1
+
+        return -1
