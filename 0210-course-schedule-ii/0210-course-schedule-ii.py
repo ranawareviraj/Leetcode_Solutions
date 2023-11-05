017531ea7c14751108495984b44f9a3d25851099
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-04 22:05:15 -0400

    Time: 89 ms (96.24%), Space: 19.1 MB (50.77%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,24 +1,31 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        prerequisites_graph = defaultdict(list)
-        prerequisites_count = [0] * numCourses
+        def finish_order(course, pre_graph, course_status, current_order):
+            if course_status[course] == IN_PROGRESS:
+                return False
+            if course_status[course] == COMPLETED:
+                return True
 
-        for x, y in prerequisites:
-            prerequisites_graph[y].append(x)
-            prerequisites_count[x] += 1
+            course_status[course] = IN_PROGRESS
+            
+            for pre_course in pre_graph[course]:
+                if not finish_order(pre_course, pre_graph, course_status, current_order):
+                    return False
+                
+            current_order.append(course)
+            course_status[course] = COMPLETED
+            return True
 
-        queue = deque()
+        pre_graph = defaultdict(list)
+        for course, pre in prerequisites:
+            pre_graph[pre].append(course)
+
+        NOT_STARTED, IN_PROGRESS, COMPLETED = 0, 1, 2
+        course_status = [NOT_STARTED] * numCourses
+
+        order = []
         for course in range(numCourses):
-            if prerequisites_count[course] == 0:
-                queue.append(course)
+            if not finish_order(course, pre_graph, course_status, order):
+                return []
 
-        result = []
-        while queue:
-            course = queue.popleft()
-            result.append(course)
-            for next_course in prerequisites_graph[course]:
-                prerequisites_count[next_course] -= 1
-                if prerequisites_count[next_course] == 0:
-                    queue.append(next_course)
-
-        return result if len(result) == numCourses else []
+        return reversed(order)
