a6676126f40249da5f16e7c28d86e8eeb67f654a
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-05 15:26:08 -0500

    Time: 109 ms (71.31%), Space: 16.5 MB (27.5%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -2,56 +2,37 @@ class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
         no_of_positions = n * n
-
-        #Pre-Processing
-        # Create position array of size n^2 and update row, col no for each position
-        positions = [None] * (no_of_positions + 1)
+        
+        positions = [ -1 ] * (no_of_positions + 1)
         position = 1
-        columns = list(range(n))
+        columns = [ col for col in range(n)]
         for row in range(n - 1, -1, -1):
             for col in columns:
-                positions[position] = (row, col)
+                positions[position] = board[row][col]
                 position += 1
             columns.reverse()
-
-        # Initialize queue with start position (pos = 0)
-        queue = deque([1])
+        
+        visited = { 1 }
+        queue = deque([ 1 ])
         level = 0
-        seen =  {1}
-
-        # BFS
+        
         while queue:
             level_size = len(queue)
-
             for _ in range(level_size):
                 current_position = queue.popleft()
-
+                
                 if current_position == no_of_positions:
                     return level
-
-                for next in range(1, 7):
-                    next_position = current_position + next
-                    if next_position <= no_of_positions:
-                        next_row, next_col = positions[next_position]
-                        if board[next_row][next_col] != -1:
-                            next_position = board[next_row][next_col]
-                            # next_row, next_col = positions[next_position]
-                        if next_position not in seen:
-                            seen.add(next_position)
-                            queue.append(next_position)
                 
+                next_positions = [ current_position + i for i in range(1, 7) if current_position + i <= no_of_positions]
+                for next_position in next_positions:
+                    if positions[next_position] != -1:
+                        next_position = positions[next_position]
+                    
+                    if next_position not in visited:
+                        visited.add(next_position)
+                        queue.append(next_position)
             level += 1
-            # For each level
-            #   - if current is in bound
-            #   - Get the next position => row, col on the board
-            #   - If snake or ladder => move to next position
-            #   - If reached target, return current level
-            # If position not visited, add it to visited set
-            # Increment level
+        
         return -1
-'''        
-[
-    [-1,-1],
-    [-1,3]
-]
-'''
+            
