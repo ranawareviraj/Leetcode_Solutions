0b2d648a414b368aba691e7282b31682135b436b
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-06 00:24:09 -0500

    Time: 883 ms (77.11%), Space: 64.9 MB (45.18%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -4,12 +4,12 @@ class Solution:
             if len(current_combination) == k:
                 combinations.append(current_combination[:])
                 return
-        
+            
             for num in range(start, n + 1):
                 current_combination.append(num)
                 backtrack(current_combination, num + 1)
                 current_combination.pop()
-        
+
         combinations = []
         backtrack([], 1)
         return combinations
