a4c9468fbe6276665a79ff720a2e5657c78ca3de
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-05 19:24:19 -0500

    Time: 975 ms (20.59%), Space: 64.7 MB (65.51%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(current_combination, start):
+            if len(current_combination) == k:
+                combinations.append(current_combination[:])
+            
+            for num in range(start, n + 1):
+                current_combination.append(num)
+                backtrack(current_combination, num + 1)
+                current_combination.pop()
+        
+        combinations = []
+        backtrack([], 1)
+        return combinations
+
+
