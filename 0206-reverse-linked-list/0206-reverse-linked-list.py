0aa7cef00fa2cc9094655bb44443a280f35476f2
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-08 21:46:10 -0500

    Time: 44 ms (44.61%), Space: 22.9 MB (6.22%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -10,8 +10,21 @@ class Solution:
         if not head.next:
             return head
         
+        # 1 -> 
+        # head = 1
+
+        # Preserve next node of the current node
+        # next_node = 2
+        # 1 -> X
         next_node = head.next
         head.next = None
+
+        # Assume reverseList function returns head reversed LL starting next node
+        # 2 <- 3 <- 4
+        # new_head = 4
         new_head = self.reverseList(next_node)
+
+        # Set the next node of the preseved node to the prev_head 
+        # X <- 1 <- 2 <- 3 <- 4
         next_node.next = head
         return new_head
