6ea944847d69b3f1542d4d871dadfc2ea68c6ba0
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-09 20:51:02 -0500

    Time: 89 ms (80.03%), Space: 17.4 MB (79.28%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -9,14 +9,16 @@ class Solution:
             while stack and (stack[-1] > 0 and asteroid < 0):
                 if abs(stack[-1]) < abs(asteroid):
                     stack.pop()
-                elif abs(asteroid) == abs(stack[-1]):
-                    stack.pop()
+                # If current asteroid is destroyed, break out of the loop
+                elif abs(asteroid) <= abs(stack[-1]):
                     is_current_destroyed = True
-                    break
-                else:
-                    is_current_destroyed = True
+                    # Before breaking, check if top us same as current,
+                    # If yes, destroy top as well
+                    if abs(asteroid) == abs(stack[-1]):
+                        stack.pop()
                     break
 
+            # If current asteroid is not yet destroyed push it onto stack
             if not is_current_destroyed:
                 stack.append(asteroid)
         
