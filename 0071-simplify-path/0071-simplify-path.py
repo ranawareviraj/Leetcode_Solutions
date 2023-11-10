d117e78b69949bbc2992a533ca04538d330c10ea
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-09 19:28:54 -0500

    Time: 42 ms (49.92%), Space: 16.5 MB (8.56%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,23 +1,14 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        tokens = path.split("/")
         stack = []
 
-        for token in tokens:
-            if token:
-                if token == "..":
-                    if stack:
-                        stack.pop()
-                elif token != ".":
-                    stack.append(token)
+        for dir in path.split("/"):
+            if dir == "..":
+                if stack:
+                    stack.pop()
+            elif not dir or dir == ".":
+                continue
+            else:
+                stack.append(dir)
         
-        # Build simplified canonical path
-        result = ["/"]
-        for token in stack:
-            result.append(token)
-            result.append("/")
-        if len(result) > 1:
-            result.pop()
-
-        return "".join(result)
-        
+        return "/" + "/".join(stack)
