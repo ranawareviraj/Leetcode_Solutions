2eca0bf8657c828a743db191662e2b1718d1be70
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-09 20:39:09 -0500

    Time: 97 ms (39.61%), Space: 17.4 MB (47.9%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,22 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        n = len(asteroids)
+
+        for asteroid in asteroids:
+            is_current_destroyed = False
+            while stack and (stack[-1] > 0 and asteroid < 0):
+                if abs(stack[-1]) < abs(asteroid):
+                    stack.pop()
+                    continue
+                elif abs(asteroid) == abs(stack[-1]):
+                    stack.pop()
+                
+                is_current_destroyed = True
+                break
+
+            if not is_current_destroyed:
+                stack.append(asteroid)
+        
+        return stack
+                    
