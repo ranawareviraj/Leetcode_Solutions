ca87edd296bbb476887257cf7b0288cf42120ace
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-10 16:49:11 -0500

    Time: 52 ms (80.34%), Space: 16.4 MB (29.59%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -0,0 +1,39 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        def is_valid(r, c):
+            return (0 <= r < m) and (0 <= c < n) and grid[r][c] == 1
+
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        seen = set()
+
+        m, n = len(grid), len(grid[0])
+        fresh_oranges = 0
+        queue = deque()
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] == 1:
+                    fresh_oranges += 1
+                if grid[row][col] == 2:
+                    queue.append((row, col))
+                    seen.add((row, col))
+        
+        minutes = 0
+        while queue:
+            level_size = len(queue)
+            is_infected = False
+
+            for _ in range(level_size):
+                row, col = queue.popleft()
+                
+                for dx, dy in directions:
+                    next_row, next_col = row + dy, col + dx
+                    if is_valid(next_row, next_col) and (next_row, next_col) not in seen:
+                        is_infected = True
+                        fresh_oranges -= 1
+                        queue.append((next_row, next_col))
+                        seen.add((next_row, next_col))
+
+            if is_infected:
+                minutes += 1
+        
+        return max(minutes, 0) if fresh_oranges == 0 else -1
