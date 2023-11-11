2d20bfad680445c0e13fab32cc29cc86d2564d59
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-11 13:44:23 -0500

    Time: 140 ms (6.53%), Space: 20.7 MB (37.21%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = defaultdict(list)
+        for word in strs:
+            key = [0] * 26
+            for ch in word:
+                key[ord(ch) - ord('a')] += 1
+            
+            key = "-".join([str(num) for num in key])
+            map[key].append(word)
+        
+        return map.values()
