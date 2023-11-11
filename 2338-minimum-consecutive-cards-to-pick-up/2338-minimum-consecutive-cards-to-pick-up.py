24b5d2110dac5ade0ee97f9beff41712d9fcf995
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-11 14:23:27 -0500

    Time: 700 ms (55.52%), Space: 35.5 MB (86.2%) - LeetHub

--- 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.py
+++ 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.py
@@ -1,22 +1,15 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
         left = 0
-        curr = 0
         min_cards = math.inf
         map = defaultdict(int)
 
-        for right in range(len(cards)):
-            map[cards[right]] += 1
-
-            while map and len(map) < (right - left + 1):
-                min_cards = min(min_cards, right - left + 1)
-                
-                left_card = cards[left]
-                map[left_card] -= 1
-                if map[left_card] == 0:
-                    map.pop(left_card)
-                
-                left += 1
-        
+        for right, card in enumerate(cards):
+            if card in map:
+                left = map[card]
+                min_cards = min(right - left + 1, min_cards)
+            
+            map[card] = right
+            
         return min_cards if min_cards != math.inf else -1
 
