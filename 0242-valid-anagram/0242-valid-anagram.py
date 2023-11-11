40a636482f92c43444f3fccadbd2250ca368df05
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-10 20:40:37 -0500

    Time: 76 ms (5.34%), Space: 16.9 MB (30.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,23 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return len(Counter(s) - Counter(t)) == 0 and len(Counter(t) - Counter(s)) == 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
         s_counter = collections.Counter(s)
-        t_counter = collections.Counter(t)
-        return len(s_counter - t_counter) == 0 and len(t_counter - s_counter) == 0
-"""
+
+        for ch in t:
+            if ch not in s_counter:
+                return False
+
+            s_counter[ch] -= 1
+
+            if s_counter[ch] == 0:
+                del s_counter[ch]
+
+        return len(s_counter) == 0
