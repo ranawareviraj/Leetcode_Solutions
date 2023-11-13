6d4aa742647c8c8d38b13a48707f4ade49bf586c
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-12 22:40:56 -0500

    Time: 44 ms (53.75%), Space: 16.6 MB (54.45%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
@@ -16,28 +16,37 @@ class Solution:
             dfs(node.left, node)
             dfs(node.right, node)    
         
+        # Use DFS (or BFS) to get parent of each node
         parents = {}
         dfs(root, None)
 
+        # Start BFS from target node treating tree as undirected graph 
+        # with parent, left_child and right_child as neighbors
         queue = deque([ target ])
         level = 0
         seen = set([target])
 
+        # Using BFS traverse and find all nodes k distance away from the target node
         while queue and level < k:
+            # Size of the current level
             n = len(queue)
+
+            # Traverse current level
             for _ in range(n):
                 node = queue.popleft()
 
-                for neighbor in [node.left, node.right, parents[node]]:
+                # Push next level onto queue
+                neighbors = [ node.left, node.right, parents[node] ]
+                for neighbor in neighbors:
                     if neighbor and neighbor not in seen:
                         queue.append(neighbor)
                         seen.add(neighbor)
-
+            # Increment level
             level += 1
         
+        # From queue, capture all nodes at the current (kth) level
         result = []
         for node in queue:
             result.append(node.val)
         
         return result
-
