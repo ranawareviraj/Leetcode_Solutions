dbc852dbc109a55e260b319e435cea45916e3ddf
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-11-26 18:06:15 -0500

    Time: 65 ms, Memory: 16.5 MB - LeetHub

--- 0min-cost-climbing-stairs/0min-cost-climbing-stairs.py
+++ 0min-cost-climbing-stairs/0min-cost-climbing-stairs.py
@@ -1,10 +1,13 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
+        
+        # DP table
         dp = [math.inf] * (n + 1)
-        dp[0] = 0
-        dp[1] = 0
-
+        
+        # Base cases
+        dp[0] = dp[1] = 0
+        
         for i in range(2, n + 1):
             dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
         
