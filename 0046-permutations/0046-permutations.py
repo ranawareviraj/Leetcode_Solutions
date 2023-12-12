b2a17ff75b61aee540b81099ed5608a645d21302
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2023-12-12 18:39:15 -0500

    Time: 53 ms (9.4%), Space: 16.6 MB (26.42%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,16 +1,23 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(nums, curr, ans):
-            if len(curr) == len(nums):
-                ans.append(curr[:])
-                return
+        def backtrack(current_permutation, seen):
+            if len(current_permutation) == len(nums):
+                permutations.append(current_permutation[:])
+                # return
             
             for num in nums:
-                if num not in curr:
-                    curr.append(num)
-                    backtrack(nums, curr, ans)
-                    curr.pop()
+                if num not in seen:
+                    # Take element to the current combination
+                    current_permutation.append(num)
+                    seen.add(num)
+
+                    # Call backtrack recursively to build the combination
+                    backtrack(current_permutation, seen)
+                    
+                    # Remove the last added element from the current combination
+                    current_permutation.pop()
+                    seen.remove(num)
             
-        ans = []
-        backtrack(nums, [], ans)
-        return ans
+        permutations = []
+        backtrack([], set())
+        return permutations
