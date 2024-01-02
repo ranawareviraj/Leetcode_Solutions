a440fd0804dd8043186a19605b731d6643fcb856
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-01-01 19:15:02 -0500

    Time: 81 ms (5.03%), Space: 17.2 MB (12.02%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,6 +1,5 @@
 class Solution:
     def isPowerOfTwo(self, n):
         if n == 0: return False
-        if n % 2 == 0:
-            n = self.isPowerOfTwo(n // 2)
-        return n == 1
+        
+        return n & (-n) == n
