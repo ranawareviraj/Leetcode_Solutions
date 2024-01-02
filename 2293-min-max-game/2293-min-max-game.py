fcbd040178e4160aae768f8f75b403bbe6a66830
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-01-02 03:31:04 -0500

    Time: 100 ms (5.05%), Space: 17.5 MB (12.77%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.py
+++ 2293-min-max-game/2293-min-max-game.py
@@ -4,19 +4,17 @@ class Solution:
         n = len(arr)
 
         while n > 1:
-            is_min = True
             n //= 2
             next_arr = [0] * n
             j = 0
 
             for i in range(n):
-                if is_min:
+                if i % 2 == 0:
                     next_arr[i] = min(arr[j], arr[j + 1]) 
                 else:
                     next_arr[i] = max(arr[j], arr[j + 1]) 
-                is_min = not is_min
                 j += 2
-                
+
             arr = next_arr
 
         return arr[0]
