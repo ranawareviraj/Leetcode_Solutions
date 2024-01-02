d45deda745ef05c841042535732be5df3395f236
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-01-01 19:31:52 -0500

    Time: 35 ms (82.07%), Space: 17.2 MB (10.44%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,10 +1,10 @@
 class Powers:
     def __init__(self):
         max_power = 15
-        nums = [1] * (max_power + 1)
+        self.nums = nums = [1] * (max_power + 1)
         for i in range(1, max_power + 1):
             nums[i] = 4 * nums[i - 1]
-        self.nums = set(nums)
+        
 
 class Solution:
     p = Powers()
