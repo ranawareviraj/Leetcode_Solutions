727e75bb0fd513c6bb8e88ba2f8f3d81ba7e7a9b
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-01-01 19:31:33 -0500

    Time: 69 ms (5.06%), Space: 17.3 MB (10.44%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,9 +1,12 @@
+class Powers:
+    def __init__(self):
+        max_power = 15
+        nums = [1] * (max_power + 1)
+        for i in range(1, max_power + 1):
+            nums[i] = 4 * nums[i - 1]
+        self.nums = set(nums)
+
 class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        if n < 1:
-            return False
-        
-        while n % 4 == 0:
-            n //= 4
-        
-        return n == 1
+    p = Powers()
+    def isPowerOfFour(self, num: int) -> bool:
+        return num in self.p.nums
