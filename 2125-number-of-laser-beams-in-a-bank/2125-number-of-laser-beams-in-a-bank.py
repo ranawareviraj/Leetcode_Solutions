849e7986e385d68e6a48b39424975c123ea56ab0
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-01-02 19:43:51 -0500

    Time: 799 ms (5.01%), Space: 19.3 MB (20.41%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -1,22 +1,14 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        m, n = len(bank), len(bank[0])
-        row_wise_count = [0] * m
+        total = prev_row_count = curr_row_count = 0
 
         for i, row in enumerate(bank):
             for cell in row:
-                row_wise_count[i] += int(cell)
-        
-        total = 0
-        prev_row_count = row_wise_count[0]
-        for i in range(1, m):
-            if row_wise_count[i] > 0:
-                total += prev_row_count * row_wise_count[i]
-                prev_row_count = row_wise_count[i]
+                curr_row_count += int(cell)
+            
+            if curr_row_count > 0:
+                total += prev_row_count * curr_row_count
+                prev_row_count = curr_row_count
+                curr_row_count = 0
         
         return total
-
-'''        
-row_count => security_devices
-iterate if next one has security_devices => mul and add
-'''
