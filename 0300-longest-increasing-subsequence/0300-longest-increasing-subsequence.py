8a7039740d6ce89c62b585d2560cef65a1f47639
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-01-05 18:00:27 -0500

    Time: 75 ms (87.37%), Space: 17.7 MB (14.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,32 +1,15 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        def dp(i, memo):
-            # If result in cache, return cached result - Memoization
-            if i in memo:
-                return memo[i]
-
-            # Base case: When i = 0, below for loop wont be excuted 
-            # As it excutes for j if j is less than i that is if its less than 0
-            ans = 1
-
-            # ecursively find 
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    # If number at j is less than number at i,
-                    # so we have length = len(j) + 1(for ith number)
-                    ans = max(ans, dp(j, memo) + 1)
-            
-            # Add result to cache - Memoization
-            memo[i] = ans       
-            return ans
+        sub = [nums[0]]
         
-        max_length = 1
-        n = len(nums)
-        memo = {}
+        for num in nums[1:]:
+            if num > sub[-1]:
+                sub.append(num)
+            else:
+                # Find the first element in sub that is greater than or equal to num
+                i = 0
+                while num > sub[i]:
+                    i += 1
+                sub[i] = num
 
-        # For each index of an array,
-        # Find out max_length of increasing subsequence ending at current index
-        for i in range(n):
-            max_length = max(max_length, dp(i, memo))
-        
-        return max_length
+        return len(sub)
