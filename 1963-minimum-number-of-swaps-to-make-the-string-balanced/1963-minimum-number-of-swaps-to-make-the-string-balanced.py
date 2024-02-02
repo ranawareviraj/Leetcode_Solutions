e19cb97a4728f4c3ec2c2eae2ca9d4e0cb626da8
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-02-02 01:01:23 -0500

    Time: 224 ms (73.52%), Space: 28 MB (81.18%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,17 +1,18 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-
-
         # stack = []
         count = 0
         res = 0
+        matches = 0
         for i in s:
             if i == '[':
                 count += 1
+                matches += 1
             else:
+                matches -= 1
                 if count>0:
                     count -= 1
                     continue
                 else:
                     res += 1
-        return ceil(res/2)
+        return -1 if matches != 0 else ceil(res/2)
