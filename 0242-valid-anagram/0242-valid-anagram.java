6b090a21c1b9e32ee0a079bce25465c6afd2f3a5
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-02-17 01:28:13 -0500

    Time: 16 ms (25.16%), Space: 45.2 MB (6.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,7 +5,7 @@ class Solution {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        for(char ch : t.toCharArray()){
+        for(char ch: t.toCharArray()){
             if(!map.containsKey(ch)){
                 return false;
             }
