4773cc031594b27acf6753b9a52fc67a48b73b8d
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-03-22 01:11:37 -0400

    Time: 10 ms (26.48%), Space: 47.3 MB (87.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,13 +1,11 @@
 class Solution {
     public static List<List<String>> groupAnagrams(String[] strs) {
-        return Arrays.stream(strs)
+        return new ArrayList<>(Arrays.stream(strs)
                 .collect(Collectors.groupingBy(str -> {
                     char[] chars = str.toCharArray();
                     Arrays.sort(chars);
                     return new String(chars);
                 }))
-                .values()
-                .stream()
-                .collect(Collectors.toList());
+                .values());
     }
 }
