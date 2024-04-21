2856e5b0255f0aa5c1d979d5bc73f9b183b527e1
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-04-21 11:43:21 -0500

    Time: 39 ms (79.46%), Space: 17.7 MB (19.29%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -3,15 +3,13 @@ class Solution:
         result = []
         
         for i in range(1, n + 1):
-            message = []
-            if i % 3 == 0:
-                message.append("Fizz")
-            if i % 5 == 0:
-                message.append("Buzz")
+            message = ""
+            
+            if i % 3 == 0: message += "Fizz"
+            if i % 5 == 0: message += "Buzz"
+            
+            if not message: message += str(i)
 
-            if len(message) == 0:
-                message.append(str(i))
-
-            result.append("".join(message))
+            result.append(message)
         
         return result
