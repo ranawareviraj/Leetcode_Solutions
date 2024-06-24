08e01a99083611890d56f29dc91494a8e9050ba3
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-06-24 13:03:17 -0500

    Time: 90 ms (93.53%), Space: 18.6 MB (71.07%) - LeetHub

---
+++ 2288-apply-discount-to-prices/2288-apply-discount-to-prices.py
@@ -0,0 +1,15 @@
+class Solution:
+    def discountPrices(self, sentence: str, discount: int) -> str:
+        tokens = sentence.split()
+        m = discount / 100
+
+        for i, token in enumerate(tokens):
+            # Check if it's in correct format
+            price = token[1:]
+            if token[0] == "$" and price.isdigit():
+                discounted_price = int(price) * (1 - m)
+                formatted_price = "$" + "{:.2f}".format(discounted_price)
+
+                tokens[i] = formatted_price
+
+        return " ".join(tokens)  # Combine the updated list
