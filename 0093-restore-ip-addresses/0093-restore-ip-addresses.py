2ac280e8b9605d78989cbd5671c61b2559c17af9
Viraj Ranaware <112779376+ranawareviraj@users.noreply.github.com>
2024-09-01 21:37:30 -0500

    Time: 34 ms (84.66%), Space: 16.4 MB (88.27%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        def valid(octate):
+            return not ((octate[0] == '0' and len(octate) > 1) or int(octate) > 255)
+
+
+        def backtrack(curr, index):
+            if len(curr) > 4:
+                return
+
+            if index == len(s) and len(curr) == 4:
+                ip_addresses.append(".".join(curr[:]))
+            
+            for i in range(1, 4):
+                if index + i <= len(s):
+                    octate = s[index : index + i]
+                    if valid(octate):
+                        curr.append(octate)
+                        backtrack(curr, index + i)
+                        curr.pop()
+
+        ip_addresses = []
+        backtrack([], 0)
+        return ip_addresses
